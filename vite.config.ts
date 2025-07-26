9e15ce23c188453f6d6f93d0962e3a0552723090
Israel <israeltf1219@gmail.com>
2025-07-27 01:31:29 +0200

    para deploy netflify

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   base: './', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
